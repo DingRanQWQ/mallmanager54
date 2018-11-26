73877a426e862b60e594864022e42f14be98bf87
dr <you@example.com>
2018-11-26 17:10:44 +0800

    首页侧边栏

---
+++ src/components/home/home.vue
@@ -0,0 +1,118 @@
+<template>
+    <el-container class="container">
+  <el-header class="header">
+   <el-row>
+  <el-col :span="4"><div class="grid-content bg-purple">
+      <img src="../../assets/logo.png" alt="该图片无法显示"></div></el-col>
+  <el-col :span="19" class="middle"> <div class="grid-content bg-purple-light"><h2>电商后台管理系统</h2></div></el-col>
+  <el-col :span="1"><a href="#" class="loginout">退出</a> </el-col>
+</el-row>
+</el-header>
+
+
+  <el-container>
+
+    <el-aside class="aside" width="200px">
+       
+    <el-menu class="el-menu-vertical-demo" :router="true" :unique-opened="true">
+        <!-- 1 -->
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>用户管理</span>
+        </template> 
+           <el-menu-item index="1-1">
+                <i class="el-icon-location"></i>
+               用户列表</el-menu-item> 
+      </el-submenu>
+      <!-- 2 -->
+      <el-submenu index="2">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>权限管理</span>
+        </template> 
+           <el-menu-item index="1-1">
+                <i class="el-icon-location"></i>
+               角色列表</el-menu-item> 
+     
+       <el-menu-item index="1-1">
+                <i class="el-icon-location"></i>
+               权限列表</el-menu-item> 
+       </el-submenu>
+      <!-- 3 -->
+      <el-submenu index="3">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>商品管理</span>
+        </template> 
+           <el-menu-item index="1-1">
+                <i class="el-icon-location"></i>
+               商品列表</el-menu-item> 
+                <el-menu-item index="1-1">
+                <i class="el-icon-location"></i>
+             分类参数</el-menu-item>
+                <el-menu-item index="1-1">
+                <i class="el-icon-location"></i>
+              商品分类</el-menu-item>
+      </el-submenu>
+      <!-- 4 -->
+      <el-submenu index="4">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>订单管理</span>
+        </template> 
+           <el-menu-item index="1-1">
+                <i class="el-icon-location"></i>
+               订单列表</el-menu-item> 
+      </el-submenu>
+      <!-- 5 -->
+      <el-submenu index="5">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>数据统计</span>
+        </template> 
+           <el-menu-item index="1-1">
+                <i class="el-icon-location"></i>
+               数据报表</el-menu-item> 
+      </el-submenu>
+
+
+    </el-menu>
+        </el-aside>
+
+
+    <el-main class="main">Main</el-main>
+  </el-container>
+</el-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.container{
+    height: 100%;
+}
+.header {
+  background-color: #b3c0d1;
+}
+.header .middle {
+    color: #fff;
+    text-align: center;
+}
+.header .loginout{
+    line-height: 60px;
+    text-decoration: none;
+}
+.aside {
+  background-color: #d3dce6;
+}
+
+.main {
+  background-color: #e9eef3;
+  height: 100%;
+}
+</style>
---
+++ src/components/login/login.vue
@@ -0,0 +1,70 @@
+<template>
+<div class="login-wrap">
+    
+    <el-form label-position="top" label-width="80px" :model="formdata" class="login-form">
+        <h2>用户登录</h2>
+  <el-form-item label="用户名">
+    <el-input v-model="formdata.username"></el-input>
+  </el-form-item>
+  <el-form-item label="密码">
+    <el-input v-model="formdata.password"></el-input>
+  </el-form-item>
+    <el-button @click.prevent="handleLogin" type="primary" class="login-button">登录</el-button>
+</el-form>
+</div>
+    
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    data(){
+        return {
+            formdata:{
+                username:'',
+                password:''
+            }
+        }
+    },
+    methods:{
+        async handleLogin(){
+          
+            const res =await this.$http.post('login',this.formdata)
+            //console.log(res)
+                const 
+                {
+                    data,
+                    meta:{status,msg}
+                }=res.data
+                if(status===200){
+                    const token = localStorage.setItem('token',data.token)
+                    this.$message.success(msg);  
+                    this.$router.push({name:'home'}) 
+                }else{
+                    this.$message.error(msg);
+                }
+            
+        }
+    }
+}
+</script>
+
+<style>
+.login-wrap{
+    height: 100%;
+    background-color: #324152;
+    display:flex;
+    justify-content: center;
+    align-items: center;
+}
+.login-wrap .login-form{
+    background-color: #fff;
+    width: 400px;
+    padding: 30px;
+    border-radius: 5px;
+}
+.login-wrap .login-form .login-button{
+    width: 100%;
+}
+
+</style>
